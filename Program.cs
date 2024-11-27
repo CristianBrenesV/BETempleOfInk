c22a5f4099b959a2d427a151a8dd00f24c61d252
CristianBrenesV <brenescris1@gmail.com>
2024-11-27 11:05:44 -0600

    Program.cs

--- Program.cs
+++ Program.cs
@@ -16,20 +16,18 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configurar la tubería de solicitud HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
-        c.RoutePrefix = string.Empty; // Swagger UI en la raíz
+        c.RoutePrefix = string.Empty; 
     });
 }
 
 app.UseHttpsRedirection();
 
-// Configurar el uso de controladores
 app.MapControllers();
 
 app.Run();
